a205e1384be545035a1179a15d07afb42a960f13
Echo G <ChaosExAnima@users.noreply.github.com>
2019-06-27 14:39:42 -0400

    Reverts to using Express.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('Hello!');
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.json({ message: 'hello!' });
+});
+
+app.listen(port, () => console.log(`App listening on port ${port}`));
